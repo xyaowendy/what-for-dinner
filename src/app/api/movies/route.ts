631b177f43da3db0bf81ad08da9bdbe184c358a9
Wendy Jiang <wendyjiang0611@gmail.com>
2024-08-02 21:06:15 +1000

    connect mongo db

---
+++ src/app/api/movies/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import clientPromise from '@/lib/mongodb';
+
+export async function GET() {
+    try {
+        const client = await clientPromise;
+        const db = client.db("sample_mflix");
+        const movies = await db
+            .collection("movies")
+            .find({})
+            .sort({ metacritic: -1 })
+            .limit(10)
+            .toArray();
+        return NextResponse.json(movies);
+    } catch (e) {
+        console.error(e);
+        return NextResponse.error();
+    }
+}
